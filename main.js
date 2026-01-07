0dcfa6ac011c500612fa863dd5f6d2a4f431d35a
Elahe <elahemotaghizade@gmail.com>
2026-01-07 11:24:38 +0330

    fix minOdd

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /** کوچکترین عدد فرد یک ارایه عددی 20 ایتمی */
 
-var numbers=[3,7,12,34,1,5,9,6,13,16,20,53,66,90,13,22,17,18,19,20];
-var minOdd=1;
-for(var i=1;i<numbers.length;i++){
-    if((numbers[i]%2!==0)&&(numbers[i]<minOdd)){
+var numbers=[3,7,12,34,5,9,6,13,16,20,53,66,90,13,22,17,18,19,20];
+var minOdd=Infinity;
+for(var i=0;i<numbers.length;i++){
+    if(numbers[i]%2!==0 && numbers[i]<minOdd){
         minOdd=numbers[i];
     }
 }
